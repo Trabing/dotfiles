879abed117ce757e97f2724af8609a4078575302
Kenso Trabing <ktrabing@acm.org>
2018-12-05 02:10:22 -0500

    Extend Oh My ZSH

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 sudo apt-get -y install zsh \
-powerline \
-fonts-powerline \
-zsh-theme-powerlevel9k \
-zsh-syntax-highlighting
+    powerline \
+    fonts-powerline \
+    zsh-syntax-highlighting
+
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
